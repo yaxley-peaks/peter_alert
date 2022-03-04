18703526e4aaded055fea57e288f562993bf77e0
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-03-04 19:38:55 +0530

    seperate icon

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::{BufWriter, Write};
 use image_base64;
+use base64;
 pub const PATH: &str = "./peter.png";
 pub const IMG_STRING: &str = include_str!("./peter.txt");
 
@@ -16,4 +17,13 @@ pub fn prepare_image(){
     let img_file = fs::File::create(PATH).unwrap();
     let mut sink = BufWriter::new(img_file);
     sink.write(&img).unwrap();
+}
+
+pub fn get_ico(){
+    let mut img = include_str!("icon.txt").to_string(); 
+    let offset = img.find(",").unwrap();
+    let value = img.drain(..offset);
+    let bytes = base64::decode(value).unwrap();
+    let mut file = fs::File::create("icon.ico").unwrap();
+    file.write(&bytes).unwrap();
 }
